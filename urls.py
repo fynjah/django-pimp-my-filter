d5df6b167cd55e3665f94803599cc1ed5d4fdf53
fynjah <fynjah@gmail.com>
2013-05-21 14:09:07 +0300

    cleaning

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from filter_manager import views
-
-urlpatterns = patterns('',
-   url(r'^get_structure/$', views.get_structure,),
-   url(r'^save_filter/$', views.save_filter,),
-   url(r'^get_typeahead/$', views.get_typeahead,),
-)
