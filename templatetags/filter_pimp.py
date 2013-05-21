35e582d859d3fe47e9da1e75051c99e4c3dcd328
fynjah <fynjah@gmail.com>
2013-05-21 14:09:55 +0300

    cleaning

--- templatetags/filter_pimp.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.simple_tag
-def pimp_tag(app):
-		return '<div id="pimp-my-tag" rel="%s" style="display:none;"></div>' % app
