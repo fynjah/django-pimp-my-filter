7006862ca05ae770a680762533ef69bcfc115265
fynjah <fynjah@gmail.com>
2013-05-21 14:04:47 +0300

    setup.py

---
+++ filter_manager/README.md
@@ -0,0 +1,10 @@
+django-pimp-my-filter
+=====================
+
+Use it at your own risk! Alpha-dev version, as is.
+
+Required: Twitter Bootsrap 2.3+, jQuery.
+
+App designed to pimp your filter for any model, but for now it's unfinnished :)
+
+![PIMP](https://raw.github.com/fynjah/django-pimp-my-filter/master/static/Untitled.png "PIMP")
---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name = 'django-pimp-my-filter',
+    version = '0.1b',
+    packages = ['filter_manager', 'filter_manager.templatetags'],
+    include_package_data = True,
+    license = 'BSD License', # example license
+    description = 'An application, that helps you to build your own filters and use it.',
+    long_description = README,
+    url = 'https://github.com/fynjah/django-pimp-my-filter',
+    author = 'Anton Ievtushenko',
+    author_email = 'fynjah@gmail.com',
+    classifiers = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
