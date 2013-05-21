d0cd7a918fceef5c63dbd5d6bb9d40c90444d5b4
fynjah <fynjah@gmail.com>
2013-05-21 15:14:18 +0300

    setup.py fix?

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
@@ -9,10 +9,10 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name = 'django-pimp-my-filter',
     version = '0.1b',
-    packages = ['filter_manager', 'filter_manager.templatetags'],
+    packages = find_packages(),
     include_package_data = True,
     license = 'BSD License', # example license
-    description = 'An application, that helps you to build your own filters and use it.',
+    description = 'An application, that helps you build your own filters and use it.',
     long_description = README,
     url = 'https://github.com/fynjah/django-pimp-my-filter',
     author = 'Anton Ievtushenko',
